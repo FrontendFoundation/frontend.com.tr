dabb6dc2b897cb2b2caa0e371b3ecdfb19107d11
Yusuf Ulusoy <info@yusufulusoy.com.tr>
2022-07-05 02:10:54 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/frontend.com.tr',
+  assetPrefix: '/frontend.com.tr',
   experimental: {
     images: {
       unoptimized: true,
